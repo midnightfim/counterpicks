66ee1eaf0705b4ad208ce29aac1908b1063e666a
Yaroslav <yskarma1@mts.ru>
2023-07-08 23:39:33 +0300

    COUN-6 created nest workspace

--- apps/server-e2e/jest.config.ts
+++ apps/server-e2e/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'api-e2e',
+  displayName: 'server-e2e',
   preset: '../../jest.preset.js',
   globalSetup: '<rootDir>/src/support/global-setup.ts',
   globalTeardown: '<rootDir>/src/support/global-teardown.ts',
@@ -15,5 +15,5 @@ export default {
     ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/api-e2e',
+  coverageDirectory: '../../coverage/server-e2e',
 };
